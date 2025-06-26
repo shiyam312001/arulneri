bc6c1a903ec5098b38f6f400fac03b8dc151b895
Shiyam <shiyamlawrence@redantlabs.com>
2025-06-26 16:47:43 +0530

    changes

--- app/components/HomeContact.js
+++ app/components/HomeContact.js
@@ -57,7 +57,7 @@ export default function HomeContact() {
     <div className='home-contact-form'>
       <div className='container'>
         <div className='row contact-row'>
-          <p className='contact-head-title'>"More than treatment — We offer Love and Care"</p>
+          <p className='contact-head-title'>&quot;More than treatment — We offer Love and Care&quot;</p>
           <div className='col-md-6'>
             <div className="banner-img-ref">
               <Image
